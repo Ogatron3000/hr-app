5e4ec1ea8fa6469c63431bd02a00b9b65622fd8f
Ivan Mijanovic <ivanmijanovic18@gmail.com>
2021-03-26 22:49:24 +0100

    alpine-init to separate script and removed flatpickr

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,79 +13,15 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
+        <!-- Alpine Init -->
+        <script src="{{ asset('js/alpine-init.js') }}" defer></script>
+
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
 
         {{-- Temporary: Flatpickr --}}
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-        <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
-
-        {{-- Temporary: Alpine Interractivity --}}
-        <script>
-            function data() {
-                function getThemeFromLocalStorage() {
-                    // if user already changed the theme, use it
-                    if (window.localStorage.getItem('dark')) {
-                        return JSON.parse(window.localStorage.getItem('dark'))
-                    }
-
-                    // else return their preferences
-                    return (
-                        !!window.matchMedia &&
-                        window.matchMedia('(prefers-color-scheme: dark)').matches
-                    )
-                }
-
-                function setThemeToLocalStorage(value) {
-                    window.localStorage.setItem('dark', value)
-                }
-
-                return {
-                    dark: getThemeFromLocalStorage(),
-                    toggleTheme() {
-                        this.dark = !this.dark
-                        setThemeToLocalStorage(this.dark)
-                    },
-                    isSideMenuOpen: false,
-                    toggleSideMenu() {
-                        this.isSideMenuOpen = !this.isSideMenuOpen
-                    },
-                    closeSideMenu() {
-                        this.isSideMenuOpen = false
-                    },
-                    isNotificationsMenuOpen: false,
-                    toggleNotificationsMenu() {
-                        this.isNotificationsMenuOpen = !this.isNotificationsMenuOpen
-                    },
-                    closeNotificationsMenu() {
-                        this.isNotificationsMenuOpen = false
-                    },
-                    isProfileMenuOpen: false,
-                    toggleProfileMenu() {
-                        this.isProfileMenuOpen = !this.isProfileMenuOpen
-                    },
-                    closeProfileMenu() {
-                        this.isProfileMenuOpen = false
-                    },
-                    isPagesMenuOpen: false,
-                    togglePagesMenu() {
-                        this.isPagesMenuOpen = !this.isPagesMenuOpen
-                    },
-                    // Modal
-                    isModalOpen: false,
-                    trapCleanup: null,
-                    openModal() {
-                        this.isModalOpen = true
-                        this.trapCleanup = focusTrap(document.querySelector('#modal'))
-                    },
-                    closeModal() {
-                        this.isModalOpen = false
-                        this.trapCleanup()
-                    },
-                }
-            }
-        </script>
-
+        {{--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">--}}
+        {{--<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>--}}
     </head>
     <body class="font-sans antialiased">
         <div class="flex h-screen bg-gray-50 dark:bg-gray-900" :class="{ 'overflow-hidden': isSideMenuOpen }">
@@ -100,10 +36,4 @@
             </div>
         </div>
     </body>
-    <script>
-        flatpickr('#birthdate', {maxDate: 'today'});
-        flatpickr('#joined', {maxDate: 'today'});
-        flatpickr('#date', {maxDate: 'today'});
-        flatpickr('#expiry', {maxDate: 'today'});
-    </script>
 </html>
